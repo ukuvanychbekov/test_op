a3ae7259b7eb2be37b430343451ebd9f4a1b58a0
Bamboozled357 <realromankim@gmail.com>
2023-12-12 10:36:19 +0600

    enterprise_service_fix

--- app/models/enterprise_token.rb
+++ app/models/enterprise_token.rb
@@ -25,6 +25,103 @@
 #
 # See COPYRIGHT and LICENSE files for more details.
 #++
+# class EnterpriseToken < ApplicationRecord
+#   class << self
+#     def current
+#       RequestStore.fetch(:current_ee_token) do
+#         set_current_token
+#       end
+#     end
+
+#     def table_exists?
+#       connection.data_source_exists? table_name
+#     end
+
+#     def allows_to?(action)
+#       Authorization::EnterpriseService.new(current).call(action).result
+#     end
+
+#     def active?
+#       current && !current.expired?
+#     end
+
+#     def show_banners?
+#       OpenProject::Configuration.ee_manager_visible? && !active?
+#     end
+
+#     def set_current_token
+#       token = EnterpriseToken.order(Arel.sql('created_at DESC')).first
+
+#       if token&.token_object
+#         token
+#       end
+#     end
+#   end
+
+#   validates :encoded_token, presence: true
+#   validate :valid_token_object
+#   validate :valid_domain
+
+#   before_save :unset_current_token
+#   before_destroy :unset_current_token
+
+#   delegate :will_expire?,
+#            :subscriber,
+#            :mail,
+#            :company,
+#            :domain,
+#            :issued_at,
+#            :starts_at,
+#            :expires_at,
+#            :reprieve_days,
+#            :reprieve_days_left,
+#            :restrictions,
+#            to: :token_object
+
+#   def token_object
+#     load_token! unless defined?(@token_object)
+#     @token_object
+#   end
+
+#   def allows_to?(action)
+#     Authorization::EnterpriseService.new(self).call(action).result
+#   end
+
+#   def unset_current_token
+#     # Clear current cache
+#     RequestStore.delete :current_ee_token
+#   end
+
+#   def expired?(reprieve: true)
+#     token_object.expired?(reprieve:) || invalid_domain?
+#   end
+
+#   ##
+#   # The domain is only validated for tokens from version 2.0 onwards.
+#   def invalid_domain?
+#     return false unless token_object&.validate_domain?
+
+#     !token_object.valid_domain?(Setting.host_name)
+#   end
+
+#   private
+
+#   def load_token!
+#     @token_object = OpenProject::Token.import(encoded_token)
+#   rescue OpenProject::Token::ImportError => e
+#     Rails.logger.error "Failed to load EE token: #{e}"
+#     nil
+#   end
+
+#   def valid_token_object
+#     errors.add(:encoded_token, :unreadable) unless load_token!
+#   end
+
+#   def valid_domain
+#     errors.add :domain, :invalid if invalid_domain?
+#   end
+# end
+
 class EnterpriseToken < ApplicationRecord
   class << self
     def current
@@ -38,15 +135,15 @@ class EnterpriseToken < ApplicationRecord
     end
 
     def allows_to?(action)
-      Authorization::EnterpriseService.new(current).call(action).result
+      true
     end
 
     def active?
-      current && !current.expired?
+      true
     end
 
     def show_banners?
-      OpenProject::Configuration.ee_manager_visible? && !active?
+      false
     end
 
     def set_current_token
@@ -84,7 +181,7 @@ class EnterpriseToken < ApplicationRecord
   end
 
   def allows_to?(action)
-    Authorization::EnterpriseService.new(self).call(action).result
+    true
   end
 
   def unset_current_token
@@ -93,15 +190,13 @@ class EnterpriseToken < ApplicationRecord
   end
 
   def expired?(reprieve: true)
-    token_object.expired?(reprieve:) || invalid_domain?
+    false
   end
 
   ##
   # The domain is only validated for tokens from version 2.0 onwards.
   def invalid_domain?
-    return false unless token_object&.validate_domain?
-
-    !token_object.valid_domain?(Setting.host_name)
+    false
   end
 
   private
